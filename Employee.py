c8b321901ccd3c6eaca5760cf0558eea44ebc1e5
divyakumar95 <divyakumardiv@gmail.com>
2021-11-19 22:49:36 +0000

    Fixed Classes and types

--- Employee.py
+++ Employee.py
@@ -1,81 +1,91 @@
 import pandas as pd
 
 class Employee:
-    def __init__(self, StafID=None, EmplDb=None, StaffDb=None):
-        self.OtRate = 24              #From Data
-        self.ST = 0.2                 #20% - Standard Tax 
-        self.HT = 0.4                 #40% - Higher Tax   
-        self.StaffID = StafID
-        self.EmplDb = EmplDb
-        self.StaffDb = StaffDb
-        if self.EmplDb == None:                     #check for external file source 
-            self.EmplDb = './DataFiles/Employees.txt'
-            self.Employee_data = pd.read_csv(self.EmplDb, sep=" ", header=0, index_col=False)
-        elif self.EmplDb != None:
-            self.Employee_data = pd.read_csv(self.EmplDb, sep=" ", header=0, index_col=False)
-        else:
-            #print('Please fill the table fully if unknown specify 0',Error)
-            raise Exception('Invalid file path or buffer object type')
-        self.EmpDB()    
-        if self.StaffID is None and self.StaffDb is None:  #check if no specific Employee
-            self.StaffDb = 'DataFiles/Hours.txt'
-            Hours_data = pd.read_csv(self.StaffDb, sep=" ", header=0, index_col=False)
-            self.HoursWork = [HW for HW in Hours_data['HoursWorked']]
-            self.date = [DA for DA in Hours_data['Date']]
-            self.StafID = [StaffID for StaffID in Hours_data['StaffID']]
-            for i in range(len(self.StafID)):
-                self.StaffID = self.StafID[i]
-                self.HoursWorked = self.HoursWork[i]
-                self.Date = self.date[i]
-                comp = self.ComputePayment(self.HoursWorked, self.Date)
-                print(comp)
-        
-    def EmpDB(self):
-        #Employee_data = pd.read_csv(self.EmployeeDB, sep=" ", header=0, index_col=False)
-        self.RefID = [RD for RD in self.Employee_data['StaffID']]
-        self.LastName = [LN for LN in self.Employee_data['LastName']]
-        self.FirstName = [FN for FN in self.Employee_data['FirstName']]
-        self.RegHr = [RH for RH in self.Employee_data['RegHours']]
-        self.HourlyRate = [HR for HR in self.Employee_data['HourlyRate']]
-        self.OTM = [OTM for OTM in self.Employee_data['OTMultiple']]
-        self.TaxCredit = [TC for TC in self.Employee_data['TaxCredit']]
-        self.StdBnd = [SB for SB in self.Employee_data['StandardBand']]
+    def __init__(self, RefID, LastName, FirstName, RegHr, HourlyRate, OTM, TaxCredit, StdBnd):
+        self.RefID = RefID
+        self.LastName = LastName
+        self.FirstName = FirstName
+        self.RegHr = RegHr
+        self.HourlyRate = HourlyRate
+        self.OTM = OTM
+        self.TaxCredit = TaxCredit
+        self.StdBnd = StdBnd
+        self.OtRate = 24                #From Data
+        self.ST = 0.2                   #20% - Standard Tax 
+        self.HT = 0.4                   #40% - Higher Tax  
 
     def ComputePayment(self, HoursWorked=None , Date=None):  
-        self.HoursWorked = HoursWorked
+        self.HoursWorked = int(HoursWorked)
         self.Date = Date
-        #print(self.StaffID, self.HoursWorked, self.Date)
-        for i in range(len(self.RefID)):
-            if self.StaffID == self.RefID[i]:
-                if self.HoursWorked > self.RegHr[i]:            # Check for Overtime 
-                    overTime = self.HoursWorked - self.RegHr[i]
-                else:
-                    overTime = 0
-                overTimePay = overTime * self.OtRate
-                RegularRate = self.RegHr[i] * self.HourlyRate[i]
-                GrossPay = (self.HourlyRate[i] * self.RegHr[i]) + (overTimePay)
-                if GrossPay > self.StdBnd[i]:                   # Check for High Payment Rate 
-                    HighPayBand = GrossPay - self.StdBnd[i]
-                else:
-                    HighPayBand = 0
-                StandardTax = self.StdBnd[i] * self.ST
-                HighTax = HighPayBand * self.HT
-                TotalTax = StandardTax + HighTax
-                Name = self.FirstName[i] +' '+ self.LastName[i]
-                NetDeductions = self.TaxCredit[i] + HighTax
-                if GrossPay != 0:
-                    NetPay = GrossPay - NetDeductions
-                else:
-                    raise ValueError('Regular hour or hourly rate can not be zero')
-                Date = self.Date
+        if self.HoursWorked > self.RegHr:                           # Check for Overtime 
+            overTime = self.HoursWorked - self.RegHr
+            RegHourworked = self.RegHr
+        else:
+            overTime = 0
+            RegHourworked = self.HoursWorked
+        overTimePay = overTime * self.OtRate
+        RegularRate = self.RegHr * self.HourlyRate
+        GrossPay = (self.HourlyRate * self.RegHr) + (overTimePay)
+        if GrossPay > self.StdBnd:                                  # Check for High Payment Rate 
+            HighPayBand = GrossPay - self.StdBnd
+        else:
+            HighPayBand = 0
+        StandardTax = self.StdBnd * self.ST
+        HighTax = HighPayBand * self.HT
+        TotalTax = StandardTax + HighTax
+        Name = self.FirstName +' '+ self.LastName
+        NetDeductions = self.TaxCredit + HighTax
+        if GrossPay != 0:
+            NetPay = GrossPay - NetDeductions
+        else:
+            raise ValueError('Regular hour or hourly rate can not be zero')
+        Date = self.Date
                 
-                return {'Name': Name, 'Date':Date, 'Regular Hours Worked': self.RegHr[i],
-                'Overtime Hours Worked':overTime,'Regular Rate':self.HourlyRate[i],
+        return {'Name': Name, 'Date':Date, 'Regular Hours Worked': RegHourworked,
+                'Overtime Hours Worked':overTime,'Regular Rate':self.HourlyRate,
                 'Overtime Rate':self.OtRate, 'Regular Pay':RegularRate, 'Overtime Pay':overTimePay,
-                'Gross Pay':GrossPay, 'Standard Rate Pay':self.StdBnd[i],'Higher Rate Pay':HighPayBand,
-                'Standard Tax':round(StandardTax,2),'Higher Tax':round(HighTax,2), 'Total Tax': round(TotalTax,2), 'Tax Credit':self.TaxCredit[i],
+                'Gross Pay':GrossPay, 'Standard Rate Pay':self.StdBnd,'Higher Rate Pay':HighPayBand,
+                'Standard Tax':round(StandardTax,2),'Higher Tax':round(HighTax,2), 'Total Tax': round(TotalTax,2), 'Tax Credit':self.TaxCredit,
                 'Net Deductions':NetDeductions, 'Net Pay': NetPay}
+
+def main(EmplDb=None, StaffDb=None):
+    if EmplDb == None and StaffDb is None:                     #check for external file source 
+        EmplDb = './DataFiles/Employees.txt'
+        StaffDb = './DataFiles/Hours.txt'
+        Employee_data = pd.read_csv(EmplDb, sep=" ", header=0, index_col='StaffID')
+        Hours_data = pd.read_csv(StaffDb, sep=" ", header=0, index_col='StaffID')
+    elif EmplDb != None and StaffDb != None:
+        Employee_data = pd.read_csv(EmplDb, sep=" ", header=0, index_col='StaffID')
+        Hours_data = pd.read_csv(StaffDb, sep=" ", header=0, index_col='StaffID')
+    else:
+        raise Exception('Invalid file path or buffer object type')
+    for index, row in Hours_data.iterrows():
+        x = Employee_data.loc[[index]]
+        Emp = Employee(index, x.iloc[0]['LastName'], x.iloc[0]['FirstName'], x.iloc[0]['RegHours'], x.iloc[0]['HourlyRate'], x.iloc[0]['OTMultiple'], x.iloc[0]['TaxCredit'], x.iloc[0]['StandardBand'])
+        comp = Emp.ComputePayment(row.HoursWorked,str(row.Date))
+        print(comp)
+
+def createFile():
+    EmpDB = """StaffID LastName FirstName RegHours HourlyRate OTMultiple TaxCredit StandardBand
+12345 Green Joe 37 16 1.5 70 700
+12346 Gi Joe 33 17 1.5 60 600
+12347 Chi Bing 45 20 2 65 900
+12348 Mark Bud 37 19 0 90 500
+12349 Ji Joe 30 05 0 12 121"""
+
+    HourDB = """StaffID Date HoursWorked
+12345 31/10/2021 42
+12348 30/11/2021 20
+12345 31/09/2021 05
+12349 30/09/2021 30"""
+
+    with open('./DataFiles/Employees.txt','w') as f:
+        f.write(EmpDB)
+
+    with open('./DataFiles/Hours.txt','w') as f:
+        f.write(HourDB)
 
 
 if __name__ == '__main__':
-    Employee() 
+    createFile()
+    main()
